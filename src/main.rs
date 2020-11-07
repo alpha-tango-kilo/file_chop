2c5d8a878d5e236559754cdba8e8d9928ca71351
alpha-tango-kilo <atk@aaathats3as.com>
2020-11-07 22:26:08 +0000

    Redo 'clap' App in Rust Allows for use of validators

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,39 @@
 extern crate clap;
-use clap::{App, load_yaml};
+use clap::{
+    Arg,
+    App,
+    //load_yaml,
+};
 
 fn main() {
-    let yaml = load_yaml!("./args/en-gb.yml");
-    let matches = App::from(yaml).get_matches();
+    //let yaml = load_yaml!("./args/en-gb.yml");
+    //let matches = App::from(yaml).get_matches();
+    //println!("{:#?}", matches);
+
+    let matches = App::new("file_chop")
+        .arg(Arg::with_name("CHOP")
+            .short("C")
+            .conflicts_with("PLOP"))
+        .arg(Arg::with_name("PLOP")
+            .short("P")
+            .conflicts_with("CHOP"))
+        .arg(Arg::with_name("number")
+            .short("n")
+            .long("number")
+            .takes_value(true)
+            .conflicts_with_all(&["PLOP", "size"])
+            .requires("CHOP")
+            .validator(|s| {
+                match s.parse::<usize>() {
+                    Ok(_) => Ok(()),
+                    Err(_) => Err("Failed to parse unsigned integer for number of parts to chop file into".into()),
+                }
+            }))
+        .arg(Arg::with_name("file")
+            .required(true)
+            .min_values(1))
+        .get_matches();
+
     println!("{:#?}", matches);
+    println!("{:?}", matches.values_of("file").unwrap());
 }
