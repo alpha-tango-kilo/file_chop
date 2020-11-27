c30a987d433b54191117d9f23587cb5def064940
alpha-tango-kilo <atk@aaathats3as.com>
2020-11-27 21:03:39 +0000

    Customer error messages and an allocation save

--- src/lib.rs
+++ src/lib.rs
@@ -21,16 +21,18 @@ pub fn chop(path: &str, part_count: &u64, slice_size: &u64) -> Result<()> {
         .open(path)?;
     let mut file_size = file.metadata()?.len();
     let mut buffer = vec![];
-    let mut part_count = part_count.clone();
+    let mut part_count = *part_count;
 
     while part_count > 0 {
         // Can't slice from end as it's not guaranteed we can make a u64 an i64
         file.seek(SeekFrom::Start(file_size - slice_size)).unwrap_or_else(|_| { eprintln!("Failed to seek"); 0 });
-        file.read_to_end(&mut buffer)?;
+        file.read_to_end(&mut buffer)
+            .context("Failed to read part of file")?;
         let mut write_file = OpenOptions::new()
             .create_new(true) // Fail if there's already a file there, we don't want to mess things up
             .write(true)
-            .open(format!("{}.chop{}", path, part_count))?;
+            .open(format!("{}.chop{}", path, part_count))
+            .context("Failed to create a chop part file (does it already exist?)")?;
         write_file.write_all(&buffer)?;
 
         file_size -= slice_size;
